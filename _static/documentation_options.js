6bf6e4ab9937f9accd2cdc8f1422e09239a0e73d
Markus Heiser <markus.heiser@darmarit.de>
2023-03-21 17:38:09 +0100

    gh-pages: updated

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20221127',
+    VERSION: '20230321',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
