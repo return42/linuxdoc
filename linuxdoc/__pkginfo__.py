f5bcaf6829b88387dac3f8c62ef359c888ae2182
Markus Heiser <markus.heiser@darmarit.de>
2026-05-04 10:26:10 +0200

    [release] linuxdoc v20260504
        $ git tag -a 20260504 -m 'Version 20260504'
        $ git push origin 20260504
    
        # https://pypi.org/project/linuxdoc/
        $ hatch build -c
        $ hatch publish ./dist/linuxdoc-20260504*
    
        # https://return42.github.io/linuxdoc/
        $ hatch run doc:man
        $ hatch run ./prj doc.gh-pages

--- linuxdoc/__pkginfo__.py
+++ linuxdoc/__pkginfo__.py
@@ -2,8 +2,8 @@
 # pylint: disable=invalid-name, # pylint: disable=attribute-defined-outside-init
 """About ``linuxdoc``"""
 
-__version__ = "20240924.dev1"
-__copyright__ = "2025 Markus Heiser"
+__version__ = "20260504"
+__copyright__ = "2026 Markus Heiser"
 
 __url__ = "https://github.com/return42/linuxdoc"
 __docs__ = "https://return42.github.io/linuxdoc"
